ea2dff2d0ceda000766deca0c1a404b4db11a589
shubham <shubhamqweasd@gmail.com>
2016-09-29 20:48:58 +0530

    unit test setup

---
+++ public/tests/specs/test.spec.ts
@@ -0,0 +1,10 @@
+/// <reference path="../../../typings/globals/jasmine/index.d.ts" />
+
+import { TestBed } from '@angular/core/testing';
+
+describe('App', () => {
+  
+  it ('should work', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ public/tests/webpack.test.js
@@ -0,0 +1,25 @@
+module.exports = {
+  devtool: 'inline-source-map',
+
+  resolve: {
+    extensions: ['', '.ts', '.js']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loaders: ['ts']
+      },
+      {
+        test: /\.html$/,
+        loader: 'html'
+
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'null'
+      }
+    ]
+  }
+}
